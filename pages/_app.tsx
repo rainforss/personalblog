e5c77436778a3ef2e09c1df0b323accb94239cde
rainforss <shengyan@ualberta.ca>
2021-08-03 11:46:41 -0600

    Moved theme file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import "@fontsource/saira-semi-condensed";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme";
+import theme from "../utils/theme";
 
 function SafeHydrate({ children }) {
   return (
